40bebd0c4e67206f2ec79a09d64261c158677eb0
Mahvish Sadaf <mahvishsadaf953@yahoo.com>
2024-03-12 23:30:50 +0530

    Updates

---
+++ courses2.js
@@ -0,0 +1,38 @@
+const courses = [
+    {
+        id: 1,
+        title: "Accelerated Student Memory Mastery",
+        author: "Dr. Rama Mehta",
+        platform: "Udemy",
+        description: "Memory Mastery, Memory Retention, Enhancing Self-Esteem & Confidence",
+        image: "https://img-b.udemycdn.com/course/240x135/3695326_2d39_3.jpg",
+        link: "https://www.udemy.com/course/accelerated-student-memory-mastery/learn/lecture/23763580?start=0#overview",
+        rating: 4.5,
+        learners: 1000
+    },
+    {
+        id: 2,
+        title: "Arduino",
+        author: "Free Code Camp",
+        platform: "Youtube",
+        description: "Learn Python - Full Course for Beginners [Tutorial]",
+        image: "https://i.ytimg.com/vi/zJ-LqeX_fLU/hqdefault.jpg",
+        link: "https://www.youtube.com/embed/rfscVS0vtbw",
+        rating: 3.5,
+        learners: 1654
+    },
+    {
+        id: 3,
+        title: "Machine Learning with Python",
+        author: "Free Code Camp",
+        platform: "Free Code Camp",
+        description: "TensorFlow framework is used to build several neural networks and explore more advanced techniques like natural language processing and reinforcement learning.",
+        image: "https://i.ytimg.com/vi/tPYj3fFJGjk/hqdefault.jpg",
+        link: "https://www.freecodecamp.org/learn/machine-learning-with-python/",
+        rating: 3.5,
+        learners: 1654
+    }
+    // Add more course objects here
+];
+
+export default courses;
